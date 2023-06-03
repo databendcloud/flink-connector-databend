9b85c66fb896c84159575cf0e0a280a64ddcbc0d
hantmac <hantmac@outlook.com>
2023-06-03 11:18:32 +0800

    fix test

--- src/test/java/org/apache/flink/TestDatabendOutputFormat.java
+++ src/test/java/org/apache/flink/TestDatabendOutputFormat.java
@@ -14,6 +14,7 @@ import org.apache.flink.table.data.StringData;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.types.RowKind;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
