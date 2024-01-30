a3cd658802b8d82dbeddea3c1d5460b2802c9454
hantmac <hantmac@outlook.com>
2024-01-30 17:11:41 +0800

    fix typo

--- src/main/java/org/apache/flink/connector/databend/catalog/databend/DatabendSystem.java
+++ src/main/java/org/apache/flink/connector/databend/catalog/databend/DatabendSystem.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Doris System Operate.
+ * Databend System Operate.
  */
 @Public
 public class DatabendSystem implements Serializable {
